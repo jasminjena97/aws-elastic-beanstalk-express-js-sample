1ac1673ec9e74932b1509a15d2030d20a8daaf13
Jasmini Jena <97896786+jasminjena97@users.noreply.github.com>
2023-02-08 17:48:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to my github account!'));
+app.get('/', (req, res) => res.send('Simple project with code pileline done!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
